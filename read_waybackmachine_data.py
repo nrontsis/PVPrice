5bf7ada9d5ab7468f5d6c8123a9173b2c696010a
nrontsis <nrontsis@gmail.com>
2019-08-25 22:20:38 +0100

    Nice figure for thin films

--- read_waybackmachine_data.py
+++ read_waybackmachine_data.py
@@ -3,6 +3,8 @@ import numpy as np
 from urllib.request import Request, urlopen
 import os
 import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+import matplotlib.dates as mdates
 
 # req = Request('https://www.energytrend.com/solar-price.html', headers={'User-Agent': 'Mozilla/5.0'})
 # webpage = urlopen(req).read()
@@ -37,7 +39,6 @@ def extract_prices(df):
     assert(high_price > mean_price)
     return high_price, low_price, mean_price
 
-
 df = pd.DataFrame(columns=['timestamp', 'thinfilm_high', 'thinfilm_low', 'thinfilm_mean'])
 i = 0
 for root, dirs, files in os.walk("websites/"):
@@ -54,9 +55,32 @@ for root, dirs, files in os.walk("websites/"):
                     i += 1
             except ValueError:
                 print("!!!!NO TABLES FOUND!!!!")
-
 df = df.sort_values('timestamp')
-plt.semilogy(df['timestamp'], df['thinfilm_mean'])
-# plt.semilogy(df['timestamp'], df['thinfilm_low'])
-plt.show()
-import pdb; pdb.set_trace()
+df = df.set_index('timestamp')
+df.to_pickle('data.pkl')
+
+
+df = pd.read_pickle('data.pkl')
+
+def plot_prices(time, mean, high, low):
+    fig = plt.figure(figsize=(6, 3.5))
+    ax = fig.add_subplot(111)
+    ax.semilogy(df.index, df['thinfilm_mean'], 'b', lw=2)
+    ax.fill_between(df.index, df['thinfilm_low'], df['thinfilm_high'], color='blue', alpha=0.2)
+    ax.yaxis.set_major_locator(ticker.LogLocator(base=10, subs=np.arange(1, 10)+0.5))
+    ax.yaxis.set_major_formatter(ticker.ScalarFormatter())
+    ax.yaxis.set_minor_formatter(ticker.NullFormatter())
+    years = mdates.YearLocator()   # every year
+    yearsFmt = mdates.DateFormatter('%Y')
+    ax.xaxis.set_major_locator(years)
+    ax.xaxis.set_major_formatter(yearsFmt)
+    # months = mdates.MonthLocator()  # every month
+    # ax.xaxis.set_minor_locator(months)
+    plt.grid(True, 'both')
+    plt.xlabel('Time')
+    plt.ylabel('Price ($/Watt)')
+    plt.title('Historical Prices of Thin Film Module Prices (PVInsights.com)')
+    return fig
+
+fig = plot_prices(df.index, df['thinfilm_mean'], df['thinfilm_high'], df['thinfilm_low'])
+fig.savefig("thin_film.pdf", bbox_inches='tight')
